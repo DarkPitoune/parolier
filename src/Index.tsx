4eaa54492fd7e488b83d6e77c25c79b59088a8a6
Pitoune <pierredheb@gmail.com>
2024-07-26 19:51:23 +0200

    fix safaro

--- src/Index.tsx
+++ src/Index.tsx
@@ -74,7 +74,7 @@ function Index() {
     <div>
       <div className="sticky top-0 bg-white">
         <div className="bg-jubilateBlue-500 px-6 py-4 gap-4 flex justify-between items-center">
-          <div className="flex bg-white flex-1 rounded-full h-fit w-fit pl-2 gap-1 items-center">
+          <div className="flex bg-white flex-1 rounded-full w-fit pl-2 gap-1 items-center">
             <SearchIcon className="w-5 fill-jubilateBlue-500" />
             <input
               className="w-full h-9 rounded-full px-2 outline-none bg-white"
